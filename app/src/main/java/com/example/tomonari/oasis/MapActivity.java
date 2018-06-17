945ba6aa7697aab39bd7753ba61ceeb49fe7584f
tomonarifeehan <feehan.tomonari@gmail.com>
2018-06-17 14:07:13 -0400

    Populated google map with dark blue pins for purity reports and light blue pins for source reports.

--- app/src/main/java/com/example/tomonari/oasis/MapActivity.java
+++ app/src/main/java/com/example/tomonari/oasis/MapActivity.java
@@ -10,8 +10,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private User user = new User();
@@ -43,8 +54,55 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng sydney = new LatLng(-33.852, 81.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        createReportPins(googleMap);
+    }
+
+    public void createReportPins(final GoogleMap googleMap) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference.child("source_reports");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot templateSnapshot : dataSnapshot.getChildren()){
+                    for(DataSnapshot snap : templateSnapshot.getChildren()){
+                        WaterSourceReport wsReport = (WaterSourceReport) snap.getValue(WaterSourceReport.class);
+                        double latitude = Double.parseDouble(wsReport.getLocation().split(",")[0]);
+                        double longitude = Double.parseDouble(wsReport.getLocation().split(",")[1]);
+                        LatLng location = new LatLng(latitude, longitude);
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(latitude, longitude))
+                                .title("Water Source Report " + wsReport.getReportNumber())
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        Query query2 = reference.child("purity_reports");
+        query2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot templateSnapshot : dataSnapshot.getChildren()){
+                    for(DataSnapshot snap : templateSnapshot.getChildren()){
+                        WaterPurityReport wpReport = (WaterPurityReport) snap.getValue(WaterPurityReport.class);
+                        double latitude = Double.parseDouble(wpReport.getLocation().split(",")[0]);
+                        double longitude = Double.parseDouble(wpReport.getLocation().split(",")[1]);
+                        LatLng location = new LatLng(latitude, longitude);
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(latitude, longitude))
+                                .title("Water Purity Report " + wpReport.getReportNumber())
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
